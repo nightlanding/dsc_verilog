83627087b06b25cd8d1596458234edcd78926f17
WANG Hongyu <aurora850@icloud.com>
2023-06-20 21:02:20 +0900

    A new 3dconv module attempt

---
+++ sample/conv3.sv
@@ -0,0 +1,13 @@
+//conv build in 3*3*3 convlution core
+//for 6*6*3 input data 
+
+module CONV3 (
+    input clk,
+    input rst,
+    input conv_en,
+    output conv_done,
+    input [7:0] conv_in,
+    output [7:0] conv_out
+);
+    
+endmodule
